46f3d6483bf9e95b04b804c7bd06d234a6c400a1
webingpro <gabriel@webingpro.com.br>
2013-04-08 18:02:59 -0300

    Added Preview

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
+ini_set('display_errors',1); 
+error_reporting(E_ALL);
+ini_set('html_errors', 'On');
 date_default_timezone_set("America/Sao_Paulo");
 require_once("MagicHTML.php");
-
 $html = new MagicHTML;
 $html->add_css_linked("preview.css");
 $content = file_get_contents("elquery-test.html");
